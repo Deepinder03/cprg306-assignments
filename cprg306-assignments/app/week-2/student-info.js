ab76383b84110b25fae0ab50d99e5c6bd1ada85b
Deepinder03 <deepinderkumar81@gmail.com>
2024-02-05 23:45:29 -0700

    week-3 deepinder

---
+++ cprg306-assignments/app/week-2/student-info.js
@@ -0,0 +1,15 @@
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Deepinder Kumar</p>
+      <Link href="https://github.com/Deepinder03/cprg306-assignments.git">
+        Link to your GitHub repository
+      </Link>
+    </div>
+  );
+}
+
+
